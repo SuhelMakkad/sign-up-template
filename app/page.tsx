a2b451c8509a4b6b1c44cafc8bb2dc8b02fa4aa1
Suhel Makkad <makadsuhel11@gmail.com>
2025-12-16 19:16:58 +0530

    First commit - project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-dvh grid grid-cols-9">
+      <div className="col-span-5 bg-amber-100"></div>
+
+      <div className="col-span-4"></div>
+    </main>
+  )
+}
