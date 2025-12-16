078b26d7ca3f175a548376a652d2ca579d79dfd8
Suhel Makkad <makadsuhel11@gmail.com>
2025-12-16 20:00:39 +0530

    Hide left section on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import { SignUpForm } from "@/components/sign-up-form"
 export default function Home() {
   return (
     <main className="h-dvh grid grid-cols-9">
-      <div className="col-span-5 bg-amber-100"></div>
+      <section className="col-span-5 bg-amber-100 lg:block hidden"></section>
 
-      <div className="col-span-4 m-auto flex flex-col gap-6">
-        <SignUpForm className="max-w-81.5 mx-auto" />
+      <section className="lg:col-span-4 col-span-full m-auto flex flex-col gap-6">
+        <SignUpForm className="max-w-81.5 mx-auto text-center lg:text-start" />
 
         <p className="text-base font-semibold text-center whitespace-nowrap py-4.5 text-secondary-foreground">
           You&apos;ll receive an email with an invite link to join.
         </p>
-      </div>
+      </section>
     </main>
   )
 }
